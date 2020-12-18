64ee58e0c2b2d2c0496a6454ffb5679d2bfb5ffe
vizartmd <vizart.md@gmail.com>
2020-12-18 17:50:56 +0200

    test servlet from browser ..

--- ServletApp/src/main/java/com/unifun/MyServlet.java
+++ ServletApp/src/main/java/com/unifun/MyServlet.java
@@ -1,10 +1,11 @@
 package com.unifun;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,18 +16,17 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter writer = response.getWriter()) {
-            writer.println("<!DOCTYPE html><html>");
-            writer.println("<head>");
-            writer.println("<meta charset=\"UTF-8\" />");
-            writer.println("<title>Buna seara, dragii mei!</title>");
-            writer.println("</head>");
-            writer.println("<body>");
-
-            writer.println("<h1>Buna seara, dragii mei!</h1>");
-            writer.println("<h1>Cum viata?</h1>");
-
-            writer.println("</body>");
-            writer.println("</html>");
+            if (request.getParameter("brand").startsWith("Nok")){
+                Phone phone1 = new Phone("Nokia", "Turquoise", 5.6, 180);
+                ObjectMapper mapper = new ObjectMapper();
+                //Converting the Object to JSONString
+                String jsonString = mapper.writeValueAsString(phone1);
+                PrintWriter out = response.getWriter();
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                out.print(jsonString);
+                out.flush();
+            }
         }
 
     }
