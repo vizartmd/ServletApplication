b65ab8099c3d9edecebfe2d8f728ac791d8b3db9
vizartmd <vizart.md@gmail.com>
2020-12-20 22:48:53 +0200

    format code

--- ServletApp/src/main/java/com/unifun/MyServlet.java
+++ ServletApp/src/main/java/com/unifun/MyServlet.java
@@ -1,32 +1,32 @@
 package com.unifun;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.google.gson.Gson;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class MyServlet extends HttpServlet {
+    private Gson gson = new Gson();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter writer = response.getWriter()) {
-            if (request.getParameter("brand").startsWith("Nok")){
-                Phone phone1 = new Phone("Nokia", "Turquoise", 5.6, 180);
-                ObjectMapper mapper = new ObjectMapper();
-                //Converting the Object to JSONString
-                String jsonString = mapper.writeValueAsString(phone1);
-                PrintWriter out = response.getWriter();
-                response.setContentType("application/json");
-                response.setCharacterEncoding("UTF-8");
-                out.print(jsonString);
-                out.flush();
-            }
+            String brand = request.getParameter("brand");
+            String color = request.getParameter("color");
+            Double diagonale = Double.parseDouble(request.getParameter("diagonale"));
+            int weight = Integer.parseInt(request.getParameter("weight"));
+            Phone phone = new Phone(brand, color, diagonale, weight);
+            String employeeJsonString = this.gson.toJson(phone);
+            PrintWriter out = response.getWriter();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            out.print(employeeJsonString);
+            out.flush();
         }
 
     }
